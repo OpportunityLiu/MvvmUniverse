ce895949d1e59bb572a136498711489ee969a9c8
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-05-01 23:24:32 +0800

    warper for frame

---
+++ Opportunity.MvvmUniverse/Views/FrameWarper.cs
@@ -0,0 +1,46 @@
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Opportunity.MvvmUniverse.Views
+{
+    public class FrameWarper : INavigationHandler
+    {
+        public FrameWarper(Frame frame)
+        {
+            if (frame == null)
+                throw new ArgumentNullException(nameof(frame));
+            this.frame = new WeakReference<Frame>(frame);
+        }
+
+        private readonly WeakReference<Frame> frame;
+
+        public Frame Frame
+        {
+            get
+            {
+                if (this.frame.TryGetTarget(out var t))
+                    return t;
+                return null;
+            }
+        }
+
+        public Navigator Parent { get; set; }
+
+        public bool CanGoBack()
+        {
+            var f = Frame;
+            if (f == null)
+                return false;
+            return f.CanGoBack;
+        }
+
+        public void GoBack()
+        {
+            var f = Frame;
+            if (f == null)
+                return;
+            f.GoBack();
+            this.RaiseCanGoBackChanged();
+        }
+    }
+}
