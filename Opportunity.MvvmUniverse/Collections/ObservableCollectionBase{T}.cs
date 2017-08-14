10a5473804162c748b02f2990cc2b4c74d45fee3
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-08-14 12:29:46 +0800

    Revert "??"
    This reverts commit b5d7c498601753556c3d94248acbc217edb837f6.

--- Opportunity.MvvmUniverse/Collections/ObservableCollectionBase{T}.cs
+++ Opportunity.MvvmUniverse/Collections/ObservableCollectionBase{T}.cs
@@ -5,8 +5,6 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml.Interop;
 
 namespace Opportunity.MvvmUniverse.Collections
 {
