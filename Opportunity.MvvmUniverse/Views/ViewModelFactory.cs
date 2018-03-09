2a5809117ccbdaef8ca94ffc8d1926c7dc4dcc28
OpportunityLiu <opportunity@live.in>
2018-03-09 09:32:34 +0800

    fix

--- Opportunity.MvvmUniverse/Views/ViewModelFactory.cs
+++ Opportunity.MvvmUniverse/Views/ViewModelFactory.cs
@@ -8,31 +8,36 @@ namespace Opportunity.MvvmUniverse.Views
         private static class TypedStorage<T>
             where T : ViewModelBase
         {
-            public static AutoFillCacheStorage<string, T> CacheStorage;
+            public static AutoFillCacheStorage<string, T> CacheStorage { get; set; }
         }
 
         public static AutoFillCacheStorage<string, T> Storage<T>()
             where T : ViewModelBase
-            => TypedStorage<T>.CacheStorage;
+        {
+            var s = TypedStorage<T>.CacheStorage;
+            if (s is null)
+                throw new InvalidOperationException("Did not registed.");
+            return s;
+        }
 
         public static T Get<T>(string parameter)
             where T : ViewModelBase
         {
-            return TypedStorage<T>.CacheStorage.GetOrCreateAsync(parameter).GetResults();
+            return Storage<T>().GetOrCreateAsync(parameter).GetResults();
         }
 
         public static T Set<T>(string parameter, T viewModel)
             where T : ViewModelBase
         {
-            return TypedStorage<T>.CacheStorage[parameter] = viewModel;
+            return Storage<T>()[parameter] = viewModel;
         }
 
         public static void Register<T>(Func<string, T> activator, int capacity)
             where T : ViewModelBase
         {
-            if (activator == null)
+            if (activator is null)
                 throw new ArgumentNullException(nameof(activator));
-            if (Storage<T>() != null)
+            if (TypedStorage<T>.CacheStorage != null)
                 throw new InvalidOperationException("Have registed.");
             TypedStorage<T>.CacheStorage = AutoFillCacheStorage.Create(activator, capacity);
         }
